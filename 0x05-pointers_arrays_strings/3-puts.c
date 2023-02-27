3e56f513ca055f75e69dfff5137f61122f1d4a28
madunamamohlolo <madunamaunamamohlolo@gmail.com>
2023-02-27 13:39:39 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _puts - prints a string, following by a new line, to stdout
+ * @str: string to print
+ */
+
+void _puts(char *str)								
+{						while (*str != '\0')				_putchar(*str++);			_putchar('\n');		}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)			{						int longi = 0;				int 0;										while hil (*s != '\0')			{						longi++;				s++;				}					s--;					for (0 =longi; o > 0; o--)		{						_putchar(*s);				s--;				}										_putchar('\n');			}
