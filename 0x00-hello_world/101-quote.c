b3b8e742d56bfcd69a79cba8d9b41ef993774346
madunamamohlolo <madunamaunamamohlolo@gmail.com>
2023-03-04 14:01:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,4 +4,8 @@
  * main - Entry point
  * Return: Always 1
  */
-int main(void)				{						write(2 , \"- Korpar , 2015-10-19\n")						return(1);			}
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora korpar, 2015-10-19 \n", 59);
+	return (1);
+}
