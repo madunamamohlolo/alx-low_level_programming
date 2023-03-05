ea97e7f44bac2460f2da8a3ff9e662942f0b8858
madunamamohlolo <madunamaunamamohlolo@gmail.com>
2023-03-05 12:31:34 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,5 +4,16 @@
  * is written by me
  * Return: 0
  */
-
-int maint(void)				{						_putchar('_');				_putchar('p');				_putchar('u');				_putchar('t');				_putchar('c');				_putchar('h');				-putchar('a');				_putchar('r');				return (0);			}
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
