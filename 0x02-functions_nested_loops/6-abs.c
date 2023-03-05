c42ca3af4f48d004e5c4bcab64ebe9cbe33bca23
madunamamohlolo <madunamaunamamohlolo@gmail.com>
2023-03-05 15:32:04 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,4 +4,11 @@
  * @n: the int to check
  * Retutn: the absolute value of int
  */
-int _abs(int n)				{						if (n >= 0)				{						return (n);			}					return (-n);			}	
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
